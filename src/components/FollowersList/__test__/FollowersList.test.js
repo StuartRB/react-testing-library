ec592f0c3c856da0c5c6417f279b6d589175fe24
Stuart Burnside <stuartburnside@Stuarts-Air.broadband>
2022-09-04 10:37:52 +0100

    before and after hooks

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -11,12 +11,28 @@ const MockFollowersList = () => {
 };
 
 describe("FollowersList", () => {
+
+    beforeAll(() => {
+        console.log("Runs once before all tests")
+    });
+
+    beforeEach(() => {
+        console.log("Runs before each test")
+    });
+
+    afterAll(() => {
+        console.log("Runs once after all tests")
+    });
+
     it('shows at least one follower from API call', async () => {
         render(<MockFollowersList />);
         const follower = await screen.findByTestId("follower-item-0");
         expect(follower).toBeInTheDocument();
     });
 
+    it('test ', async () => {
+        expect(true).toBe(true);
+    });
     // it('shows five followers from API call', async () => {
     //     render(<MockFollowersList />);
     //     const followers = await screen.findAllByTestId(/follower-item/i);
